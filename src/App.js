06ab956917f7f69b3859766ae7e670cea386d141
dann1609 <dpadilla1005@gmail.com>
2020-04-15 01:14:38 -0500

    Setup CircleCI.
    * Add .circleci/config.yml
    
    * Deploy command to s3 added.
    
    * Deploy only on master.
    
    * editing checkout.
    
    * aws-cli added.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          CircleCi properly linked.
         </p>
         <a
           className="App-link"
